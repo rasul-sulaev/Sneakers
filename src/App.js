258291de21b447dbe04e0d8602727d3ee1a4eccb
rasul-sulaev <mr.sylaev@mail.ru>
2023-04-14 00:45:05 +0300

    Сверстал Карточку товара

--- src/App.js
+++ src/App.js
@@ -11,6 +11,36 @@ function App() {
         </section>
 				<section className="section">
 					<h2 className="section__title">Все кроссовки</h2>
+					<div className="cards__list">
+						<div className="card">
+							<div className="card__img-block">
+								<button className="card__btn-favorite"></button>
+								<img className="card__img" src="/img/products/1.png" width={133} height={112}></img>
+							</div>
+							<p className="card__title">Мужские Кроссовки Nike Blazer Mid Suede</p>
+							<div className="card__bottom">
+								<div>
+									<span className="card__price-text">ЦЕНА:</span>
+									<span className="card__price">12 999 руб.</span>
+								</div>
+								<button className="card__btn-add"></button>
+							</div>
+						</div>
+						<div className="card">
+							<div className="card__img-block">
+								<button className="card__btn-favorite card__btn-favorite_active"></button>
+								<img className="card__img" src="/img/products/2.png" width={133} height={112}></img>
+							</div>
+							<p className="card__title">Мужские Кроссовки Nike Blazer Mid Suede</p>
+							<div className="card__bottom">
+								<div>
+									<span className="card__price-text">ЦЕНА:</span>
+									<span className="card__price">12 999 руб.</span>
+								</div>
+								<button className="card__btn-add card__btn-add_active"></button>
+							</div>
+						</div>
+					</div>
 				</section>
       </main>
     </div>
